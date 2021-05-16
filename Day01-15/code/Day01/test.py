7cab4c9fe2bf9ad2604f40357a262545f7d010a8
num@dell <glory4148@gmail.com>
2021-05-16 11:50:11 +0800

    from home pc

--- Day01-15/code/Day01/test.py
+++ Day01-15/code/Day01/test.py
@@ -10,8 +10,21 @@
 #     print('%d / %d = %f' % (a, b, a / b))
 #     print('%d // %d = %d' % (a, b, a // b))
 #     print('%d %% %d = %d' % (a, b, a % b))
-#     print('%d ** %d = %d' % (a, b, a ** b))
-#
-#
+#     print('%d ** %d = %d' % (a, b, a ** Thank you.
 # calc(a,b)
+#-------------------------------------
+# 将华氏温度转换为摄氏温度
+# 提示：华氏温度到摄氏温度的转换公式为：$C=(F - 32) \div 1.8$
 
+# f_tempture = float(input("请输入华氏温度： "))
+# c_tempture = (f_tempture-32)/1.8
+# print('%.1f华氏度 = %.1f摄氏度' %(f_tempture, c_tempture))
+# -------------------------------------
+
+
+# 输入半径
+# 输入圆的半径计算计算周长和面积
+# f_radius = float(input("请输入圆的半径： "))
+# f_circle = 3.14 * 2 * f_radius
+# f_squre = 3.14 * (f_radius **2)
+# print("半径为%.2fm的圆的周长是%.2fm, 面积是%.2f㎡" %(f_radius, f_circle,f_squre))
